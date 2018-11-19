ef0cb7c1dfca32d1bbcc6ab20bbe8b2cea14132c
Zawar Shahid <zawar_shahid93@hotmail.com>
2018-11-19 15:33:15 +0500

    production env variables changed

--- config/env/production.js
+++ config/env/production.js
@@ -26,7 +26,8 @@ module.exports = {
    ***************************************************************************/
 
   // port: 1337,
-  port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
+  port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+  ip: process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
@@ -42,11 +43,12 @@ module.exports = {
   },
   mongodbServer: {
     adapter: 'sails-mongo',
-    host: process.env.OPENSHIFT_MONGODB_DB_HOST, //'localhost',
-    port: process.env.OPENSHIFT_MONGODB_DB_PORT, //27017,
-    user: process.env.OPENSHIFT_MONGODB_DB_USERNAME || '', //optional => superOwner
-    password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD || '', //optional => superOwner
-    database: 'pmt' //optional
+    host: process.env.OPENSHIFT_MONGODB_DB_HOST || process.env.MONGO_HOST, //'localhost',
+    port: process.env.OPENSHIFT_MONGODB_DB_PORT || process.env.MONGO_PORT, //27017,
+    user: process.env.OPENSHIFT_MONGODB_DB_USERNAME || process.env.MONGODB_USER || 'admin', //optional => superOwner
+    password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD || process.env.MONGODB_PASSWORD || '', //optional => superOwner
+    database: 'pmt',
+    url: process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
   },
 
 };

992db216e6693fe698fd2590ea8d264822bfa4b0
Zawar Shahid <zawar_shahid93@hotmail.com>
2018-12-28 17:04:29 +0500

    configurations changed

--- config/local.js
+++ config/local.js
@@ -12,7 +12,8 @@ module.exports = {
   //     cert: fs.readFileSync(__dirname + '/ssl/cert.pem')
   // },
   // port: 1337,
-  port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-  ip: process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+  // port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+  // ip: process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+  ip: '0.0.0.0'
   // explicitHost: '109.203.126.97'
 }

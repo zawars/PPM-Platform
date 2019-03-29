a81495981f2065ac8faec85de61d72728694007b
Zawar Shahid <zawar_shahid93@hotmail.com>
2019-03-29 14:52:28 +0500

    pm2 config added git push

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [{
+    name: 'PPM',
+    script: "app.js",
+    instances: "4",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    },
+    watch: true
+  }]
+}

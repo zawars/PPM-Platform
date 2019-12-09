89dd927149c79fcce0d836f5e2d7a29fb2e70e77
muzamil.bukhari.39 <muzamil.bukhari.39@gmail.com>
2019-12-09 12:01:06 +0500

    Sockets pagination for Notifications

--- config/sockets.js
+++ config/sockets.js
@@ -52,18 +52,18 @@ module.exports.sockets = {
 
 
 
- /***************************************************************************
-  *                                                                          *
-  * Whether to expose a 'get /__getcookie' route with CORS support that sets *
-  * a cookie (this is used by the sails.io.js socket client to get access to *
-  * a 3rd party cookie and to enable sessions).                              *
-  *                                                                          *
-  * Warning: Currently in this scenario, CORS settings apply to interpreted  *
-  * requests sent via a socket.io connection that used this cookie to        *
-  * connect, even for non-browser clients! (e.g. iOS apps, toasters, node.js *
-  * unit tests)                                                              *
-  *                                                                          *
-  ***************************************************************************/
+  /***************************************************************************
+   *                                                                          *
+   * Whether to expose a 'get /__getcookie' route with CORS support that sets *
+   * a cookie (this is used by the sails.io.js socket client to get access to *
+   * a 3rd party cookie and to enable sessions).                              *
+   *                                                                          *
+   * Warning: Currently in this scenario, CORS settings apply to interpreted  *
+   * requests sent via a socket.io connection that used this cookie to        *
+   * connect, even for non-browser clients! (e.g. iOS apps, toasters, node.js *
+   * unit tests)                                                              *
+   *                                                                          *
+   ***************************************************************************/
 
   // grant3rdPartyCookie: true,
 
@@ -106,10 +106,10 @@ module.exports.sockets = {
   * app's security.                                                          *
   *                                                                          *
   ***************************************************************************/
-  // beforeConnect: function(handshake, cb) {
+  // beforeConnect: function (handshake, cb) {
   //   // `true` allows the connection
   //   return cb(null, true);
-  //
+
   //   // (`false` would reject the connection)
   // },
 
@@ -136,6 +136,6 @@ module.exports.sockets = {
   * transport should be disabled.                                            *
   *                                                                          *
   ***************************************************************************/
-  // transports: ["polling", "websocket"]
+  transports: ["polling1", "websocket1"]
 
 };

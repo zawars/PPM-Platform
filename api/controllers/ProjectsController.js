23d85e59e93d32d2521684d4183503a091c0142f
Saad Afzaal <saadafzal@akkastech.com>
2020-01-24 19:37:19 +0500

    Task 189 - add overAllStatus variable in outlineApproval model

--- api/controllers/ProjectsController.js
+++ api/controllers/ProjectsController.js
@@ -439,6 +439,7 @@ module.exports = {
         let temp = {
           projectOutline: project.projectOutline[0],
           status: "Open",
+          overAllStatus: "Submitted",
           assignedTo: body.projectOutline.pmoOfficer.id,
           project: projectResponse.id,
           docType: "Outline",
@@ -484,6 +485,7 @@ module.exports = {
           OutlineApproval.create({
             projectOutline: backup,
             status: "Open",
+            overAllStatus: "Submitted",
             assignedTo: outline.pmoOfficer.id,
             project: projectResponse[0].id,
             docType: "Outline",
--- api/models/OutlineApproval.js
+++ api/models/OutlineApproval.js
@@ -11,6 +11,9 @@ module.exports = {
     status: {
       type: 'string'
     },
+    overAllStatus: {
+      type: 'string'
+    },
     projectOutline: {
       type: 'json'
     },

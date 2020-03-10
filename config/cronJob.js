d9bd78ea3362377052741b82e2658ef2c638e4c7
Saad Afzaal <saadafzal@akkastech.com>
2020-03-10 12:50:22 +0500

    Task # 365 - Create a function for email reminder of project closing report creation

--- config/cronJob.js
+++ config/cronJob.js
@@ -4,10 +4,15 @@ function cronJob(cb) {
   //Synchronize Users
   userController.syncUsers();
 
+  // sent email reminder for project closing report creation
+  userController.emailReminderClosingReport();
+
   //Synchronize Users Cycle after every 24 Hours
   let intervalTimer = 1000 * 60 * 60 * 24;
   setInterval(() => {
     userController.syncUsers();
+
+    userController.emailReminderClosingReport();
   }, intervalTimer);
 };
 

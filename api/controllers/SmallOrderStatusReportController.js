e8803edaf09efe2729ea0ee8753d3cd804e5ff8e
muzamil.bukhari.39 <muzamil.bukhari.39@gmail.com>
2020-05-08 16:09:49 +0500

    Fixed small bug in Order status reports

--- api/controllers/SmallOrderStatusReportController.js
+++ api/controllers/SmallOrderStatusReportController.js
@@ -9,11 +9,21 @@ module.exports = {
 
   getPreviousCurrentOrderReport: async (req, res) => {
     try {
-      let smallOrder = await SmallOrder.findOne({ id: req.params.id }).populateAll();
-      let prevStatusReport = await SmallOrderStatusReport.findOne({ id: req.params.prev }).populateAll();
-      let currentStatusReport = await SmallOrderStatusReport.findOne({ id: req.params.current }).populateAll();
+      let smallOrder = await SmallOrder.findOne({
+        id: req.params.id
+      }).populateAll();
+      let prevStatusReport = await SmallOrderStatusReport.findOne({
+        id: req.params.prev
+      }).populateAll();
+      let currentStatusReport = await SmallOrderStatusReport.findOne({
+        id: req.params.current
+      }).populateAll();
 
-      res.ok({smallOrder, prevStatusReport, currentStatusReport});
+      res.ok({
+        smallOrder,
+        prevStatusReport,
+        currentStatusReport
+      });
     } catch (error) {
       ErrorsLogService.logError('Small Order Status Report', `id: ${req.params.id}, ` + error.toString(), 'getPreviousCurrentOrderReport', req);
     }
@@ -21,25 +31,33 @@ module.exports = {
 
   getOrderReport: async (req, res) => {
     try {
-      let smallOrder = await SmallOrder.findOne({ id: req.params.id }).populateAll();
-      let statusReport = await SmallOrderStatusReport.findOne({ id: req.params.reportId }).populateAll();
+      let smallOrder = await SmallOrder.findOne({
+        id: req.params.id
+      }).populateAll();
+      let statusReport = await SmallOrderStatusReport.findOne({
+        id: req.params.reportId
+      }).populateAll();
 
-      res.ok({smallOrder, statusReport});
+      res.ok({
+        smallOrder,
+        statusReport
+      });
     } catch (error) {
       ErrorsLogService.logError('Small Order Status Report', `id: ${req.params.id}, ` + error.toString(), 'getOrderReport', req);
     }
   },
 
-  getStatusReportsByOrder: (req, res) => {
-    SmallOrderStatusReport.find({
-      smallOrder: req.params.id
-    }).then(reports => {
-      res.ok(reports);
-    }).catch(err => {
+  getStatusReportsByOrder: async (req, res) => {
+    try {
+      let smallOrderStatusReports = await SmallOrderStatusReport.find({
+        smallOrder: req.params.id
+      }).populateAll();
+
+      res.ok(smallOrderStatusReports);
+    } catch (err) {
       ErrorsLogService.logError('Small Order Status Reports', `id: ${req.params.id}, ` + err.toString(), 'getStatusReportsByOrder', req);
       res.badRequest(err);
-    });
+    };
   },
 
 };
-

c8efcd8d797e861cc76579ab9802daefd56c07b9
muzamilbukhari <muzamil.bukhari.39@gmail.com>
2021-05-03 16:59:19 +0500

    Removed unneeded api from gantt report

--- api/controllers/GanttMilestonesReportController.js
+++ api/controllers/GanttMilestonesReportController.js
@@ -6,10 +6,6 @@
  */
 
 module.exports = {
-    createGanttMilestoneReport: async (req, res) => {
-
-    },
-
     getGanttMilestoneReportByProject: async (req, res) => {
         let ganttMilestoneReport = await GanttMilestonesReport.find({ projectId: req.params.id });
         res.ok(ganttMilestoneReport);

d0deeaf5b6c433ba1af25b9575a8d911ed5dd89a
muzamilbukhari <muzamil.bukhari.39@gmail.com>
2021-10-13 16:53:37 +0500

    Created api for vacation mode

---
+++ api/controllers/VacationModeController.js
@@ -0,0 +1,12 @@
+/**
+ * VacationModeController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+

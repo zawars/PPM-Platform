6b9029405a999d0190d8511208c08d38873d74d6
muzamilbukhari <muzamil.bukhari.39@gmail.com>
2021-10-14 15:19:45 +0500

    Modified vacation api

--- api/controllers/VacationModeController.js
+++ api/controllers/VacationModeController.js
@@ -8,7 +8,12 @@
 module.exports = {
     getLastActiveVacationRecord: async (req, res) => {
         let user = req.params.user;
-        let lastVacationRecord = await VacationMode.findOne({ isVacationActive: true, user });
+        let todaysDate = new Date()
+        let offset = todaysDate.getTimezoneOffset()
+        todaysDate = new Date(todaysDate.getTime() - (offset * 60 * 1000))
+        todaysDate = todaysDate.toISOString().split('T')[0]
+
+        let lastVacationRecord = await VacationMode.findOne({ isVacationActive: true, user, endDate: { '>=': todaysDate }, startDate: { '<=': todaysDate } }).sort({ createdAt: -1 });
         res.ok(lastVacationRecord);
     }
 };

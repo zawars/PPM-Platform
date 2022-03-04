2c234bebace52acab09b2b46e2e6e00434444ea8
muzamilbukhari <muzamil.bukhari.39@gmail.com>
2022-03-04 16:18:05 +0500

    created api for small order gantt logs

---
+++ api/models/SmallOrderGanttLogs.js
@@ -0,0 +1,55 @@
+/**
+ * SmallOrderGanttLogs.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    smallOrderId: {
+      model: 'smallOrder',
+      required: true
+    },
+
+    action: {
+      type: 'string'
+    },
+
+    task_type: {
+      type: 'string'
+    },
+
+    taskId: {
+      type: 'string'
+    },
+    
+    taskName: {
+      type: 'string'
+    },
+
+    user: {
+      model: 'user',
+      required: true
+    },
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
